2a625cd584e15204aa1c8ca82928165ab7d57219
Eugene <me.eugenetree@gmail.com>
2026-05-10 00:29:34 +0300

    chore(db): normalize en-* language codes to en

---
+++ src/db/migrations/1778400000001-normalize-en-language-codes.ts
@@ -0,0 +1,11 @@
+import { Kysely } from 'kysely'
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.updateTable('videos')
+    .set({ language_code: 'en' })
+    .where('language_code', 'like', 'en-%')
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+}
